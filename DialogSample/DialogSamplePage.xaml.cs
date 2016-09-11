a9361d6dbf1e7ca7888adce83d37c90a98a09a1a
Tsubasa HIRANO <hirano@cuminas.jp>
2016-09-12 00:54:36 +0900

    Initial commit

---
+++ DialogSample/DialogSamplePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace DialogSample
+{
+    public partial class DialogSamplePage : ContentPage
+    {
+        public DialogSamplePage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            MessagingCenter.Send(this, "show_message", new Payload("Hello, newbie!", "May the Forms be with you..."));
+        }
+    }
+}
---
+++ DialogSample/Payload.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace DialogSample
+{
+    public class Payload
+    {
+        public Payload(string title, string message)
+        {
+            Title = title;
+            Message = message;
+        }
+
+        public string Title { get; }
+
+        public string Message { get; }
+    }
+}
